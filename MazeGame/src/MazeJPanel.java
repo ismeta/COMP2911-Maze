cb41ead089c6b4f770748ea49baac5ea03bc31b8
Oliver Tan <sinovercosequalstan@gmail.com>
2014-05-10 14:52:31 +1000

    movement

--- MazeGame/src/MazeJPanel.java
+++ MazeGame/src/MazeJPanel.java
@@ -1,10 +1,10 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -25,11 +25,15 @@ public class MazeJPanel extends JPanel {
 	/**
 	 * serialVersionUID magic
 	 */
+	private static final int REFRESH_RATE = 24;
+	private static final int NUM_PLAYERS = 3;
 	private static final long serialVersionUID = 4602880844383443785L;
 	private MazeTile[][] tiles;
 	private int size;
 	private Timer timer;
 	private MazePlayer players[];
+
+	private ConcurrentHashMap<Character, Long> keyPresses; 
 
 	/**
 	 * Generates a new MazeJPanel 
@@ -38,6 +42,8 @@ public class MazeJPanel extends JPanel {
 	public MazeJPanel(int size) {
 		// double buffered JPanel
 		super(true);
+		this.setFocusable(true);
+		
 		// generate tiles;
 		this.tiles = new MazeTile[size][size];
 		for (int i = 0; i  < size; i++) {
@@ -45,38 +51,128 @@ public class MazeJPanel extends JPanel {
 				this.tiles[i][j] = new MazeTile((new Random()).nextBoolean());
 			}
 		}
+		
 		// size
 		this.size = size;
+		
 		// allocate timer
 		this.timer = new Timer();
-		this.timer.scheduleAtFixedRate(new MazeJPanelTimer(this), 0, 1000 / 24);
+		this.timer.scheduleAtFixedRate(new MazeJPanelTimer(this), 0, 1000 / REFRESH_RATE);
+		
 		// key presses
-		this.setFocusable(true);
+		this.keyPresses = new ConcurrentHashMap<Character, Long>();
 		this.addKeyListener(new MazeJPanelKeyListener(this));
+		
+		// make da players
+		this.players = new MazePlayer[NUM_PLAYERS];
+		for (int i = 0; i < NUM_PLAYERS; i++) {
+			this.players[i] = new MazePlayer(i, this, i == 0 ? Color.red : i == 1 ? Color.blue : Color.green, 0.05);
+		}
+	}
+	
+	/**
+	 * @return the keyPresses
+	 */
+	public ConcurrentHashMap<Character, Long> getKeyPresses() {
+		return keyPresses;
+	}
+	
+	/**
+	 * @return the players
+	 */
+	public MazePlayer[] getPlayers() {
+		return players;
+	}
+
+	/**
+	 * @param key key pressed
+	 * @param time time taken
+	 */
+	public void updatePlayerMovement(char key, long time) {
+		int player;
+		int xDir = 0;
+		int yDir = 0;
+		
+		switch (key) {
+			case 'w':
+				player = 0; 
+				yDir = -1;
+				break;
+			case 'a':
+				player = 0; 
+				xDir = -1;
+				break;
+			case 's':
+				player = 0; 
+				yDir = 1;
+				break;
+			case 'd':
+				player = 0; 
+				xDir = 1;
+				break;
+			case 't':
+				player = 1; 
+				yDir = -1;
+				break;
+			case 'f':
+				player = 1; 
+				xDir = -1;
+				break;
+			case 'g':
+				player = 1; 
+				yDir = 1;
+				break;
+			case 'h':
+				player = 1; 
+				xDir = 1;
+				break;
+			case 'i':
+				player = 2; 
+				yDir = -1;
+				break;
+			case 'j':
+				player = 2; 
+				xDir = -1;
+				break;
+			case 'k':
+				player = 2; 
+				yDir = 1;
+				break;
+			case 'l':
+				player = 2; 
+				xDir = 1;
+				break;
+			default:
+				return;
+		}
+		
+		MazePlayer p = this.players[player];
+		if (p != null) {
+			p.setPosX((int) (p.getPosX() + p.getSpeed() * time * xDir));
+			p.setPosY((int) (p.getPosY() + p.getSpeed() * time * yDir));
+		}
 	}
 	
 	@Override
 	public void paint(Graphics g) {
-		System.out.println("REPAINT!");
 		super.paint(g);
 		int width = this.getWidth() / size;
 		int height = this.getHeight() / size;
-		// lol random
-		Random r = new Random();
-		Graphics2D g2d = (Graphics2D) g;
 		
-		for (int i = 0; i < this.size; i++) {
-			for (int j = 0; j < this.size; j++) {
+		// draw tiles
+		Graphics2D g2d = (Graphics2D) g;
+		for (int i = 0; i < this.tiles.length; i++) {
+			for (int j = 0; j < this.tiles[i].length; j++) {
 				this.tiles[i][j].draw(g2d, i * width, j * height, width, height);
-				// i am evil
-				if (r.nextInt(1000) > 950) {
-					this.tiles[i][j].setWall(!this.tiles[i][j].isWall());
-				}
 			}
 		}
-
-		MazePlayer mp = new MazePlayer(0);
-		mp.draw(g2d, width, height);
+		
+		// draw players
+		for (MazePlayer mp : this.players) {
+			if (mp != null) {
+				mp.draw(g2d, width, height);
+			}
+		}
 	}
 	
 	/**
@@ -84,29 +180,35 @@ public class MazeJPanel extends JPanel {
 	 * MazeJPanelTimer allows player listeners and repainting
 	 */
 	private class MazeJPanelTimer extends TimerTask {
-		private MazeJPanel mp;
+		private MazeJPanel m;
 		private long lastTime;
 		
-		private MazeJPanelTimer(MazeJPanel mp) {
-			this.mp = mp;
+		private MazeJPanelTimer(MazeJPanel m) {
+			this.m = m;
 			this.lastTime = System.currentTimeMillis();
 		}
 		
 		@Override
 		public void run() {
+			// update all the player
+			long curTime = System.currentTimeMillis();
+			for (Entry<Character, Long> e : this.m.getKeyPresses().entrySet()) {
+				long difference = curTime - e.getValue();
+	    		this.m.updatePlayerMovement(e.getKey(), difference);
+				this.m.getKeyPresses().put(e.getKey(), curTime);
+			}
 			// repaint the maze since there are updates
-			this.mp.repaint();
+			this.m.repaint();
 			// time has changed!
-			this.lastTime = System.currentTimeMillis();
+			this.lastTime = curTime;
 		}
 	}
 	
 	private class MazeJPanelKeyListener implements KeyListener {
-		private MazeJPanel mp;
-		private ConcurrentHashMap<Character, Long> kp = new ConcurrentHashMap<Character, Long>(); 
+		private MazeJPanel m;
 		
-		private MazeJPanelKeyListener(MazeJPanel mp) {
-			this.mp = mp;
+		private MazeJPanelKeyListener(MazeJPanel m) {
+			this.m = m;
 		}		
 		
 		public void keyTyped(KeyEvent e) {
@@ -114,15 +216,14 @@ public class MazeJPanel extends JPanel {
 
 	    public void keyPressed(KeyEvent e) {
 	    	if (e.getKeyChar() >= 'a' && e.getKeyChar() <= 'z') {
-	    		this.kp.put(e.getKeyChar(), System.currentTimeMillis());
-	    		System.out.println("yay");
+	    		this.m.getKeyPresses().put(e.getKeyChar(), System.currentTimeMillis());
 	    	}
 	    }
 
 	    public void keyReleased(KeyEvent e) {
 	    	if (e.getKeyChar() >= 'a' && e.getKeyChar() <= 'z') {
-	    		long difference = System.currentTimeMillis() - this.kp.remove(e.getKeyChar());
-	    		System.out.println(difference);
+	    		long difference = System.currentTimeMillis() - this.m.getKeyPresses().remove(e.getKeyChar());
+	    		this.m.updatePlayerMovement(e.getKeyChar(), difference);
 	    	}
 	    }
 	}
--- MazeGame/src/MazePlayer.java
+++ MazeGame/src/MazePlayer.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
+import java.util.Random;
 
 
 public class MazePlayer {
@@ -8,29 +9,91 @@ public class MazePlayer {
 	private int id;
 	private int posX;
 	private int posY;
+	private Color color;
+	private MazeJPanel m;
+	private double speed;
+	
 	
 	/**
 	 * @param id
+	 * @param m
+	 * @param color
+	 * @param speed
 	 */
-	public MazePlayer(int id) {
+	public MazePlayer(int id, MazeJPanel m, Color color, double speed) {
 		this.id = id;
 		this.posX = 0;
-		this.posY = 0;
+		this.posY = 200;
+		this.color = color;
+		this.m = m;
+		this.speed = speed;
 	}
 	
-	public void draw(Graphics2D g2d, int width, int height) {
-		switch (id) {
-			case 0:
-				g2d.setPaint(Color.red);
-				break;
-			case 1:
-				g2d.setPaint(Color.green);
-				break;
-			default:
-				g2d.setPaint(Color.blue);
-				break;
-		}
+	/**
+	 * @return the speed
+	 */
+	public double getSpeed() {
+		return speed;
+	}
+
+	/**
+	 * @param speed the speed to set
+	 */
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	/**
+	 * @return the color
+	 */
+	public Color getColor() {
+		return color;
+	}
+
+	public void draw(Graphics2D g2d, int width, int height) {		
+		g2d.setColor(color);
 		g2d.fill(new Ellipse2D.Double(posX, posY, width, height));
 	}
-	
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the posX
+	 */
+	public int getPosX() {
+		return posX;
+	}
+
+	/**
+	 * @param posX the posX to set
+	 */
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	/**
+	 * @return the posY
+	 */
+	public int getPosY() {
+		return posY;
+	}
+
+	/**
+	 * @param posY the posY to set
+	 */
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}	
 }
